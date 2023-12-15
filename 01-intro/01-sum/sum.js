e485c5aef976062a1f1a7b25003da6abad33fbfc
anton_vakhrushin <avvakhrushin@gmail.com>
2023-12-15 14:55:02 +0300

    #01: sum of numbers

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,12 @@
 function sum(a, b) {
-  /* ваш код */
+  const firstIsNumber = typeof a === 'number';
+  const secondIsNumber = typeof b === 'number';
+
+  if (firstIsNumber && secondIsNumber) {
+    return a + b;
+  }
+
+  throw new TypeError('Not a number');
 }
 
 module.exports = sum;
