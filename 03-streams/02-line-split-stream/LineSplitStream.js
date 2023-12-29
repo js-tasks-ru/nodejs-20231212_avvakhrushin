d8662b6f7493c5b6f8fd052c34892e096f05d752
anton_vakhrushin <avvakhrushin@gmail.com>
2023-12-29 17:00:59 +0300

    #03: 02 LineSplitStream implementation

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,14 +2,26 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
-  constructor(options) {
-    super(options);
-  }
+  #buffer = '';
 
   _transform(chunk, encoding, callback) {
+    this.#buffer += chunk.toString();
+
+    const lines = this.#buffer.split(os.EOL);
+
+    this.#buffer = lines.pop();
+    lines.forEach(line => this.push(line));
+
+    callback();
   }
 
   _flush(callback) {
+    if (this.#buffer) {
+      this.push(this.#buffer);
+      this.#buffer = '';
+    }
+
+    callback();
   }
 }
 
