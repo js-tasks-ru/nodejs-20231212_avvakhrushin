491eb0a6d6795da4bea9bf1769152b1e24ed5811
anton_vakhrushin <avvakhrushin@gmail.com>
2023-12-29 17:25:04 +0300

    #03: 03 File Server Get Implementation

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,24 +1,39 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const http = require('node:http');
+const path = require('node:path');
+const fs = require('node:fs');
+const { fileExists, hasNestedFolders, getFileExtension } = require('./file-helpers');
+const { sendErrorResponse } = require('./send-response');
+const extensions = require('./extensions');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+  const fileExtension = getFileExtension(pathname);
 
-  switch (req.method) {
-    case 'GET':
-
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+  if (req.method !== 'GET') {
+    return sendErrorResponse(res, 400, 'Bad Request.');
   }
+
+  if (hasNestedFolders(pathname)) {
+    return sendErrorResponse(res, 400, 'Nested folders are not supported.');
+  }
+
+  if (!fileExists(filepath)) {
+    return sendErrorResponse(res, 404, 'File does not exist');
+  }
+
+  const readFileStream = fs.createReadStream(filepath);
+
+  res.setHeader('Content-type', extensions[fileExtension] || 'text/plain');
+  readFileStream.pipe(res);
+
+  readFileStream.on('error', () => {
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  });
 });
 
 module.exports = server;
