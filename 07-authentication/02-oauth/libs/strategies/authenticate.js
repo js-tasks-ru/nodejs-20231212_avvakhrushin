21fead49d27f1cd785ce728625e36fae3b619420
anton_vakhrushin <avvakhrushin@gmail.com>
2024-01-26 01:44:22 +0300

    #07: remove unused debugger

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,9 +1,6 @@
 const User = require('../../models/User');
 
 module.exports = async function authenticate(strategy, email, displayName, done) {
-
-  debugger;
-
   if (!email) {
     return done(null, false, 'Не указан email');
   }
